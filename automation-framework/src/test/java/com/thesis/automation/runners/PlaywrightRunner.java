d97c86ff69cb55e78b2cc7ce720248ceac0f21d2
KarlsPortfolio <karl.aslin.portfolio@gmail.com>
2026-05-20 16:57:02 +0200

    Implemented CI-pipeline with ci.yml to enable automated tests for both Selenium/Playwright frameworks

---
+++ automation-framework/src/test/java/com/thesis/automation/runners/PlaywrightRunner.java
@@ -0,0 +1,15 @@
+package com.thesis.automation.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = "automation.playwright.stepDefinitions", // Pekar mot Playwright stepDefs
+        plugin = {"pretty", "html:target/cucumber-playwright-report.html"}
+)
+
+public class PlaywrightRunner {
+}
