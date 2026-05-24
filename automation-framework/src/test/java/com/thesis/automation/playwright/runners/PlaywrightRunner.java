5968ab67e7882704a8b6644a1b9f15923146ca36
KarlsPortfolio <karl.aslin.portfolio@gmail.com>
2026-05-24 17:14:24 +0200

    Fixed some minor spelling error in comments in the Test Runners

--- automation-framework/src/test/java/com/thesis/automation/playwright/runners/PlaywrightRunner.java
+++ automation-framework/src/test/java/com/thesis/automation/playwright/runners/PlaywrightRunner.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/features",
-        glue = "automation.playwright.stepDefinitions", // Pekar mot Playwright stepDefs
+        glue = "automation.playwright.stepDefinitions", // Pointing towards Playwright stepDefs
         plugin = {"pretty", "html:target/cucumber-playwright-report.html"}
 )
 
--- automation-framework/src/test/java/com/thesis/automation/selenium/runners/SeleniumRunner.java
+++ automation-framework/src/test/java/com/thesis/automation/selenium/runners/SeleniumRunner.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/features",
-        glue = "automation.selenium.stepDefinitions", // Pekar mot Playwright stepDefs
+        glue = "automation.selenium.stepDefinitions", // Pointing towards Selenium stepDefs
         plugin = {"pretty", "html:target/cucumber-playwright-report.html"}
 )
 public class SeleniumRunner {
