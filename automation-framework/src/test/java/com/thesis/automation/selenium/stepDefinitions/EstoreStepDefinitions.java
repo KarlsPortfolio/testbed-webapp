f96e8723a0b274378503e464ccf120de327383b3
KarlsPortfolio <karl.aslin.portfolio@gmail.com>
2026-05-26 13:38:59 +0200

    Checking to trigger CI-pipeline

--- automation-framework/src/test/java/com/thesis/automation/selenium/stepDefinitions/EstoreStepDefinitions.java
+++ automation-framework/src/test/java/com/thesis/automation/selenium/stepDefinitions/EstoreStepDefinitions.java
@@ -192,7 +192,7 @@ public class EstoreStepDefinitions {
 
     }
 
-    @When("I add the following books to my cart:")
+    @When("I add the following books to my carts:")
     public void iAddTheFollowingBooksToMyCart(List<Map<String, String>> booksTable) {
         // Write code here that turns the phrase above into concrete actions
         // 1. Loop through each row of the Gherkin table
